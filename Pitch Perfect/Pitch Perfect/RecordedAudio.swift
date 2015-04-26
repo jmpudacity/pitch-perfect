54ec22af106b8cc730a8ef76a2d33421f4abd027
jmpudacity <josemaria.pl@gmail.com>
2015-04-27 00:23:53 +0200

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,24 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by JOSE MARIA PARRA on 26/04/15.
+//  Copyright (c) 2015 jmp. All rights reserved.
+//
+
+//MODEL
+import Foundation
+
+class RecordedAudio: NSObject {
+    
+    //path
+    var filePathUrl: NSURL!
+    
+    //title
+    var title: String!
+    
+    override init() {
+        filePathUrl = nil
+        title = nil
+    }
+}
